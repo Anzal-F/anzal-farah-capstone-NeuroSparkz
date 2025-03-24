9d4ba3dcdfd92ab5fc93af020426b0ffec9c76da
Anzal-F <anzalmohamed09@gmail.com>
2025-03-24 02:03:00 -0400

    Render all the neccesary components to build the homepage

---
+++ NeuroSparkz-Client/src/pages/Homepage/Homepage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Banner from '../../components/Banner/Banner'
+import Cards from '../../components/Cards/Cards'
+import FAQs from '../../components/FAQs/FAQs'
+
+function Homepage() {
+  return (
+    <>
+      <Banner />
+      <Cards />
+      <FAQs />
+    </>
+  )
+}
+
+export default Homepage
